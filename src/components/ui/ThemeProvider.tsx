00fd2fa711c8e32953f6f52362b009b9bb253e2c
bendeandbende <tomivalmosok@gmail.com>
2024-04-02 22:49:59 +0200

    feat(common): add MUI theme

---
+++ src/components/ui/ThemeProvider.tsx
@@ -0,0 +1,10 @@
+import { createTheme } from "@mui/material";
+import { yellow } from "@mui/material/colors";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: yellow[600],
+    },
+  },
+});
